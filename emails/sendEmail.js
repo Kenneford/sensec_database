52cd0c36080d3e994c75d4af66c0f7e3950d9e2c
Patrick Kennefod Annan <100440561+Kenneford@users.noreply.github.com>
2024-11-10 07:00:14 -0100

    Email: Enrollment and sign up emails modified.

--- emails/sendEmail.js
+++ emails/sendEmail.js
@@ -6,7 +6,7 @@ const hbs = require("nodemailer-express-handlebars");
 const path = require("path");
 
 // Text message engine
-const client = require("twilio")(
+const twilioClient = require("twilio")(
   process.env.TWILIO_SID,
   process.env.TWILIO_AUTH_TOKEN,
   {
@@ -24,72 +24,88 @@ const vonage = new Vonage({
 const UserVerificationData = require("../models/user/userRefs/signUpModel/UserVerificationModel");
 
 const sendVerificationEmail = async (req, res, next) => {
-  const verificationData = req?.userVerificationData;
-  // Find user's verification data
-  const userVerificationData = await UserVerificationData.findOne({
-    userId: verificationData?.userId,
-  });
-  console.log(req?.body?.uniqueId, "L-31");
+  // Get verification data
+  const verificationData = req?.newSignedUpUserData?.verificationData;
+  // Get user's data
+  const foundStudent = req?.newSignedUpUserData?.newSignedUpUser;
+  const userPassword = req?.newSignedUpUserData?.password;
   try {
-    if (userVerificationData) {
-      // Find user who is signing up
-      const userFound = await User.findOne({
-        uniqueId: req?.body?.uniqueId,
-      }).select("+userSignUpDetails.password");
+    // Find user's verification data
+    const userVerificationData = await UserVerificationData.findOne({
+      userId: verificationData?.userId,
+    });
+    if (userFound && userFound?.contactAddress?.email) {
+      if (userVerificationData) {
+        const currentYear = new Date().getFullYear();
+        const url = "http://192.168.178.22:2025";
 
-      const currentYear = new Date().getFullYear();
-      const url = "http://localhost:3000";
+        const transporter = createGMailTransporter();
 
-      const transporter = createMailTransporter();
+        const handlebarOptions = {
+          viewEngine: {
+            extname: "hbs",
+            partialsDir: path.resolve("./emails/forSignUpVerification/"),
+            layoutsDir: path.resolve("./emails/forSignUpVerification/"),
+            defaultLayout: "",
+          },
+          viewPath: path.resolve("./emails/forSignUpVerification/"),
+          extName: ".hbs",
+        };
 
-      const handlebarOptions = {
-        viewEngine: {
-          extname: "hbs",
-          partialsDir: path.resolve("./emails/"),
-          defaultLayout: false,
-        },
-        viewPath: path.resolve("./emails/"),
-      };
+        // use a template file with nodemailer
+        transporter.use("compile", hbs(handlebarOptions));
 
-      // use a template file with nodemailer
-      transporter.use("compile", hbs(handlebarOptions));
-
-      let mailTemplate = {
-        from: `Senya Senior High School <${process.env.NODEMAILER_EMAIL}>`,
-        to: userFound?.contactAddress?.email,
-        subject: "Verify Your Email",
-        template: "email",
-        context: {
-          userImage: userFound?.personalInfo?.profilePicture.url,
-          firstName: userFound?.personalInfo?.firstName,
-          lastName: userFound?.personalInfo?.lastName,
-          company: "Sensec",
-          urlLink: `${url}/sensec/email/${userFound?.uniqueId}/${userVerificationData?.emailToken}/verify`,
-          linkText: "Verify Your Email",
-          verificationText:
-            "Verify your email by clicking the link below to sign in to your account",
-          linkExpiry: "This link expires in 1 hour",
-          currentYear,
-        },
-      };
-      transporter.sendMail(mailTemplate, (error, info) => {
-        if (error) {
-          console.log("Error sending email:", error);
-          return res
-            .status(400)
-            .json({ errorMessage: { message: "Failed to send email." } });
-        } else {
-          console.log("Verification email sent!");
-          next();
-        }
-      });
+        let mailTemplate = {
+          from: `Senya Senior High School <${process.env.NODEMAILER_GMAIL}>`,
+          to: userFound?.contactAddress?.email,
+          subject: "Verify Your Email",
+          template: "signUpVerification",
+          context: {
+            userImage: userFound?.personalInfo?.profilePicture.url,
+            uniqueId: userFound?.uniqueId,
+            firstName: userFound?.personalInfo?.firstName,
+            lastName: userFound?.personalInfo?.lastName,
+            userName: userFound?.userSignUpDetails?.userName,
+            password: userPassword,
+            company: "Senya Senior High School",
+            urlLink: `${url}/sensec/email/${userFound?.uniqueId}/${userVerificationData?.emailToken}/verify`,
+            linkText: "Verify Your Email",
+            verificationText:
+              "Verify your email by clicking the link below to sign in to your account. Please do not share your login credentials with anyone!",
+            linkExpiry: "This link expires in 1 hour",
+            currentYear,
+          },
+          attachments: [
+            {
+              filename: "school-logo.png",
+              path: path.resolve(__dirname, "assets/sensec-logo1.png"), // Path to school logo
+              cid: "schoolLogo", // Same CID as in the HTML template
+            },
+          ],
+        };
+        transporter.sendMail(mailTemplate, (error, info) => {
+          if (error) {
+            console.log("Error sending email:", error);
+            return res
+              .status(400)
+              .json({ errorMessage: { message: "Failed to send email." } });
+          } else {
+            console.log("Verification email sent!");
+            next();
+          }
+        });
+      } else {
+        return res.status(400).json({
+          errorMessage: {
+            message: "Operation failed! Could not create verification data!",
+          },
+        });
+      }
     } else {
-      return res.status(404).json({
-        errorMessage: { message: "User's verification data not found!" },
-      });
+      next();
     }
   } catch (error) {
-    return res.status(404).json({
+    return res.status(500).json({
       errorMessage: { message: "Internal server error!" },
     });
   }
@@ -122,73 +138,119 @@ async function passwordResetRequestEmail(req, res, next) {
   });
 }
 
-const sendEnrollmentEmail = async (req, res, next) => {
-  const currentUser = req.user;
-  const student = req?.enrollmentApprovalData?.studentFound;
-  let body = `
-    <div
-      style="
-        margin-bottom: 1rem;
-        padding: 2rem;
-        width: 100%;
-        padding-bottom: 2rem;
-        height: 55vh;
-        fontSize: 1.2rem;
-        border: 1px solid #ccc;
-        border-radius: .4rem;"
-    >
-    <img src=${student?.personalInfo?.profilePicture.url} alt="User Picture" 
-      style="
-        width: 4rem;
-        height: 4rem;
-        object-fit: cover;
-        border-radius: .4rem;
-      "
-    />
-  <h4>Hello ${student?.personalInfo?.firstName},</h4>
-  <h1 style="
-    color: #696969;
-    height: 4rem;
-    object-fit: cover;
-    border-radius: .4rem;
-  ">Congratulations To You...</h1>
-  <p>We're thankful once again for your enrolment into our school.</p>
-  <p>This mail is to inform you that your enrolment has been approved, and you're now a student of SENYA SENIOR HIGH SCHOOL (SENSEC).
-  <p>You can also visit our website <a href="http://localhost:3000/sensec/students/placement_check">here</a> to check for your placement into our school.</p>
-  <p>Yours Sincerely,</p>
-  <h4 style="font-weight: 600; padding-bottom: -.5rem">${currentUser?.personalInfo?.firstName} ${currentUser?.personalInfo?.lastName}</h4>
-  <p style="font-weight: 600; padding-bottom: -.5rem">Head of Administration,
-  <br>
-  Senya Senior Secondary School.</p>
-    </div>
-  `;
-  const transporter = createMailTransporter();
-  // const transporter = createGMailTransporter();
+const sendEnrollmentEmail = async ({ foundStudent }) => {
+  const currentYear = new Date().getFullYear();
+  const url = "http://192.168.178.22:2025";
+
+  const transporter = createGMailTransporter();
+
+  const handlebarOptions = {
+    viewEngine: {
+      extname: "hbs",
+      partialsDir: path.resolve("./emails/forEnrollment/"),
+      layoutsDir: path.resolve("./emails/forEnrollment/"),
+      defaultLayout: "",
+    },
+    viewPath: path.resolve("./emails/forEnrollment/"),
+    extName: ".hbs",
+  };
+
+  // use a template file with nodemailer
+  transporter.use("compile", hbs(handlebarOptions));
+
   let mailTemplate = {
-    from: `Sensec <${process.env.NODEMAILER_EMAIL}>`,
-    to: student?.contactAddress?.email,
-    subject: "Your Enrolment Status",
-    html: body,
+    from: `Senya Senior High School <${process.env.NODEMAILER_GMAIL}>`,
+    to: foundStudent?.contactAddress?.email,
+    subject: "Your Enrollment Status",
+    template: "enrollmentEmail",
+    context: {
+      userImage: foundStudent?.personalInfo?.profilePicture.url,
+      uniqueId: foundStudent?.uniqueId,
+      firstName: foundStudent?.personalInfo?.firstName,
+      lastName: foundStudent?.personalInfo?.lastName,
+      company: "Senya Senior High School",
+      urlLink: `${url}/sensec/students/${foundStudent?.uniqueId}/enrollment/online/success`,
+      linkText: "Visit Our Website",
+      currentYear,
+    },
+    attachments: [
+      {
+        filename: "school-logo.png",
+        path: path.resolve(__dirname, "assets/sensec-logo1.png"), // Path to school logo
+        cid: "schoolLogo", // Same CID as in the HTML template
+      },
+    ],
   };
   transporter.sendMail(mailTemplate, (error, info) => {
     if (error) {
-      console.log(error);
-      return res.status(400).json({
-        errorMessage: {
-          message: "Failed to send enrollment email to student!",
-        },
-      });
+      console.log("Error sending email:", error);
+      return res
+        .status(400)
+        .json({ errorMessage: { message: "Failed to send email." } });
     } else {
-      console.log("Your enrolment status has been sent to your email!");
+      console.log("Verification email sent!");
       next();
-      // res.status(200).json({
-      //   successMessage: "Enrollment email sent to student!",
-      // });
     }
   });
 };
 
-const studentEnrollmentApprovalSMS = (req, res, next) => {
+const sendEnrollmentApprovalEmail = async ({ foundStudent }) => {
+  const currentYear = new Date().getFullYear();
+  const url = "http://192.168.178.22:2025";
+
+  const transporter = createGMailTransporter();
+
+  const handlebarOptions = {
+    viewEngine: {
+      extname: "hbs",
+      partialsDir: path.resolve("./emails/forEnrollment/"),
+      layoutsDir: path.resolve("./emails/forEnrollment/"),
+      defaultLayout: "",
+    },
+    viewPath: path.resolve("./emails/forEnrollment/"),
+    extName: ".hbs",
+  };
+
+  // use a template file with nodemailer
+  transporter.use("compile", hbs(handlebarOptions));
+
+  let mailTemplate = {
+    from: `Senya Senior High School <${process.env.NODEMAILER_GMAIL}>`,
+    to: foundStudent?.contactAddress?.email,
+    subject: "Your Enrollment Status",
+    template: "enrollmentApprovalEmail",
+    context: {
+      userImage: foundStudent?.personalInfo?.profilePicture.url,
+      uniqueId: foundStudent?.uniqueId,
+      firstName: foundStudent?.personalInfo?.firstName,
+      lastName: foundStudent?.personalInfo?.lastName,
+      company: "Senya Senior High School",
+      urlLink: `${url}/sensec/students/${foundStudent?.uniqueId}/enrollment/online/success`,
+      linkText: "Visit Our Website",
+      currentYear,
+    },
+    attachments: [
+      {
+        filename: "school-logo.png",
+        path: path.resolve(__dirname, "assets/sensec-logo1.png"), // Path to school logo
+        cid: "schoolLogo", // Same CID as in the HTML template
+      },
+    ],
+  };
+  transporter.sendMail(mailTemplate, (error, info) => {
+    if (error) {
+      console.log("Error sending email:", error);
+      return res
+        .status(400)
+        .json({ errorMessage: { message: "Failed to send email." } });
+    } else {
+      console.log("Verification email sent!");
+      next();
+    }
+  });
+};
+
+const studentEnrollmentApprovalSMS = async (req, res, next) => {
   const student = req?.enrollmentApprovalData?.studentFound;
   const admin = req?.enrollmentApprovalData?.adminFound;
   let body = `
@@ -208,7 +270,7 @@ Head of Administration,
 Senya Senior High School.
 
 `;
-  client.messages
+  twilioClient.messages
     .create({
       body: body,
       // to: "491784535757", // Text your number
@@ -221,7 +283,7 @@ Senya Senior High School.
   const to = student?.contactAddress?.mobile;
   const text = body;
 
-  client.messages
+  twilioClient.messages
     .create({
       from: `whatsapp:${process.env.TWILIO_WHATSAPP_NUMBER}`,
       body: body,
@@ -246,10 +308,88 @@ Senya Senior High School.
   // }
   // sendApprovedSMS();
 };
+const userSignUpSMS = async (req, res, next) => {
+  const userInfo = req?.newSignedUpUserData?.newSignedUpUser;
+  const userPassword = req?.newSignedUpUserData?.password;
+  const url = "http://192.168.178.22:2025";
+  let body = `
+
+Hello ${userInfo?.personalInfo?.firstName},
+
+Thank you for signing up! We’re excited to have you on board and look forward to have a good experience with you.
+Below are your login credentials:
+
+Unique ID: ${userInfo?.uniqueId}
+Username: ${userInfo?.userSignUpDetails?.userName}
+Password: ${userPassword}
+
+A verification link has been sent to your email. Kindly verify your account via your email to sign in to your account.
+Please do not share your login credentials with anyone!
+
+Thank you for your understanding. Click: ${url} to visit our website to know more about SENYA SENIOR HIGH SCHOOL (SENSEC).
+
+Yours Sincerely,
+
+Patrick Kenneford Annan
+Head of Administration,
+Senya Senior High School.
+
+`;
+  try {
+    if (userInfo?.contactAddress?.mobile) {
+      twilioClient.messages
+        .create({
+          body: body,
+          // to: "491784535757", // Text your number
+          to: userInfo?.contactAddress?.mobile, // Text your number
+          from: process.env.TWILIO_NUMBER, // From a valid Twilio number
+        })
+        .then((message) => console.log(message.sid))
+        .catch((err) => console.error("Error sending sms:", err));
+
+      // twilioClient.messages.create({
+      //   from: `whatsapp:${process.env.TWILIO_WHATSAPP_NUMBER}`,
+      //   body: body,
+      //   to: "whatsapp:+4915759307163",
+      //   // to: "whatsapp:+233241283018",
+      //   // to: `whatsapp:${userInfo?.contactAddress?.mobile}`,
+      // });
+      // .then((message) => console.log(message.sid));
+      //   const from = process.env.VONAGE_BRAND_NAME;
+      //   const to = userInfo?.contactAddress?.mobile;
+      //   const text = body;
+
+      //   async function sendSMS() {
+      //     await vonage.sms
+      //       .send({ to, from, text })
+      //       .then((resp) => {
+      //         console.log("Message sent successfully");
+      //         console.log(resp);
+      //       })
+      //       .catch((err) => {
+      //         console.log("There was an error sending the messages.");
+      //         console.error(err);
+      //       });
+      //   }
+      //   sendSMS();
+      next();
+    } else {
+      next();
+    }
+  } catch (error) {
+    return res.status(500).json({
+      errorMessage: {
+        message: "Internal Server Error!",
+      },
+    });
+  }
+};
 
 module.exports = {
   sendVerificationEmail,
   passwordResetRequestEmail,
   sendEnrollmentEmail,
+  sendEnrollmentApprovalEmail,
   studentEnrollmentApprovalSMS,
+  userSignUpSMS,
 };
