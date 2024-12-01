041c2d99386df17c5980a556d64ff2e09455200b
Patrick Kennefod Annan <100440561+Kenneford@users.noreply.github.com>
2024-12-01 00:47:33 -0100

    Update server.js

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ const start = async (req, res) => {
   try {
     // Configure CORS options if needed
     const corsOptions = {
-      origin: "*", // or '*' to allow all origins
+      origin: "https://sensec-database.onrender.com/", // or '*' to allow all origins
       methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
       // allowedHeaders: "Content-Type,Authorization",
     };
@@ -102,13 +102,6 @@ const start = async (req, res) => {
     const port = process.env.PORT || 7006;
 
     app.use(express.static("public"));
-    const path = require("path");
-    app.use(express.static(path.join(__dirname, "dist"))); // Make sure this points to the correct build directory
-
-    // Catch-all route to send back index.html for all other routes
-    app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "dist", "index.html")); // Serve index.html for SPA
-    });
     // Routes
     app.use(
       "/api/sensec_db/v1",
