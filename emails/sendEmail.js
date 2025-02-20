710965bd0e204d531bc6e0edf2835edcf5c40dbd
Patrick Kennefod Annan <100440561+Kenneford@users.noreply.github.com>
2025-02-20 09:16:14 +0100

    Working on resolving password reset email issue.

--- emails/sendEmail.js
+++ emails/sendEmail.js
@@ -122,8 +122,9 @@ const sendVerificationEmail = async (req, res, next) => {
 async function passwordResetRequestEmail(req, res, next) {
   const user = req?.data?.userFound;
   const token = req?.data?.token;
-  console.log(user);
-  const url = process.env.EMAIL_URL;
+  // console.log(user);
+  const url = "https://senyashs.com";
+  // const url = process.env.EMAIL_URL;
   let body = `<h4>Hello ${user?.userSignUpDetails?.userName},</h4><p>To reset your password, kindly <a href="${url}/sensec/password/${user?.uniqueId}/${user?._id}/${token}/reset"> click here</a> to do so.</p>`;
   const transporter = nodemailer.createTransport({
     service: "gmail",
@@ -138,7 +139,7 @@ async function passwordResetRequestEmail(req, res, next) {
     subject: "Password Reset Link",
     html: body,
   };
-  transporter.sendMail(mailTemplate, (error, info) => {
+  transporter?.sendMail(mailTemplate, (error, info) => {
     if (error) {
       console.log("Error sending password reset email:", error);
       return res
